a626a3c2aa34b765ceac7b06afd78969495802ca
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-04-04 10:55:50 +0800

    Fixing the prod

--- src/components/Allproducts/AllProduct.jsx
+++ src/components/Allproducts/AllProduct.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import AllProductsCard from "./Allproductscard";
+import AllProductsCard from "./AllProductsCard";
 
 function AllProduct() {
   const [products, setProducts] = useState();
